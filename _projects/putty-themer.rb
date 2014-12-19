fd8bce88ed55fd011aa346212886c1a2fc7acba4
Stephen Orr <steve@stephenorr.co.uk>
2014-12-19 15:49:58 +0000

    Create putty-themer.rb

---
+++ _projects/putty-themer.rb
@@ -0,0 +1,7 @@
+---
+---
+A very simple command-line tool to apply a colour scheme and font to all PuTTY sessions
+saved in the user's registry. I built this about 3 years ago, and I was pleased to find
+that it still works under current versions of Windows and Ruby. I haven't been able to
+find a similar tool that does the job better (do people just not care about this stuff
+on Windows?), but do please let me know if you do find something.
